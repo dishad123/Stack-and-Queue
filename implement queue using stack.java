b29360acf512e41717e83e23d84d7b0ab2b936c8
dishad123 <94884272+dishad123@users.noreply.github.com>
2022-03-03 13:37:19 +0530

    Create implement queue using stack.java

---
+++ implement queue using stack.java
@@ -0,0 +1,45 @@
+class MyQueue {
+    Stack<Integer> input=new Stack();
+    Stack<Integer>output=new Stack();
+
+    public MyQueue() {
+        
+    }
+    
+    public void push(int x) {
+        input.push(x);
+    }
+    
+    public int pop() {
+        while(output.empty())
+            while(input.empty()==false){
+                output.push(input.peek());
+                input.pop();
+            }
+        int x=output.peek();
+        output.pop();
+        return x;
+    }
+    
+    public int peek() {
+        while(output.empty())
+            while(input.empty()==false){
+                output.push(input.peek());
+                input.pop();
+         }
+        return output.peek();
+    }
+    
+    public boolean empty() {
+        return input.empty()&&output.empty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
